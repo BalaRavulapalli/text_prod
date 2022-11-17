baa261615191433dee718c5904153f9a4c415b0e
BalaRavulapalli <balaravulapalli@gmail.com>
2022-11-17 01:45:39 +0000

    before merge to main

--- static/checkbox.js
+++ static/checkbox.js
@@ -3,19 +3,19 @@ window.addEventListener("DOMContentLoaded", function(event){
     document.getElementById("count0").value = 0;
     document.getElementById("count0").setAttribute("type", "number");
     document.getElementById("count0").setAttribute("min", "0");
-    document.getElementById("count0").setAttribute("max", "10");
+    document.getElementById("count0").setAttribute("max", "50");
     document.getElementById("div0").setAttribute("style", "margin-bottom: .5rem !important;");
     
     document.getElementById("count1").value = 0;
     document.getElementById("count1").setAttribute("type", "number");
     document.getElementById("count1").setAttribute("min", "0");
-    document.getElementById("count1").setAttribute("max", "10");
+    document.getElementById("count1").setAttribute("max", "50");
     document.getElementById("div1").setAttribute("style", "margin-bottom: .5rem !important;");
     
     document.getElementById("count2").value = 0;
     document.getElementById("count2").setAttribute("type", "number");
     document.getElementById("count2").setAttribute("min", "0");
-    document.getElementById("count2").setAttribute("max", "10");
+    document.getElementById("count2").setAttribute("max", "50");
     document.getElementById("div2").setAttribute("style", "margin-bottom: .5rem !important;");
     
     document.getElementById("count3").value = 0;
